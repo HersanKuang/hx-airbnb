c77887142a39fd7fffe44b3a54fbfceb0993bde9
Hersan Kuang <mlnrmsw@gmail.com>
2023-01-15 22:35:50 +0800

    feat: configuration theme

--- src/assets/theme/index.ts
+++ src/assets/theme/index.ts
@@ -1,11 +1,11 @@
 const theme = {
   color: {
-    primaryColor: "#ff385c",
-    secondaryColor: "#00848A"
+    primaryColor: '#ff385c',
+    secondaryColor: '#00848A'
   },
   text: {
-    primaryColor: "#484848",
-    secondaryColor: "#222"
+    primaryColor: '#484848',
+    secondaryColor: '#222'
   },
   mixin: {
     boxShadow: `
@@ -13,7 +13,7 @@ const theme = {
       &:hover {
         box-shadow: 0 2px 4px rgba(0,0,0,.18);
       }
-    `  
+    `
   }
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,23 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 
 import App from './App'
 import 'normalize.css'
 import '@/assets/css/index.less'
 import store from '@/store'
+import theme from '@/assets/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <Suspense fallback="">
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ThemeProvider>
     </BrowserRouter>
   </Suspense>
 )
